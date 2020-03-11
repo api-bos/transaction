53ea3de2c47a3d61422ba1acacd45251cfc66b42
Steven Kristianto <stevenkristianto97@yahoo.com>
2020-03-11 13:49:35 +0700

    start develop
    copy source code dari lokal to github

---
+++ src/test/java/com/bos/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bos.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
