d10caf9afe09a63ab456096fdd3167ad60944821
Steven Kristianto <stevenkristianto97@yahoo.com>
2020-03-16 14:57:24 +0700

    + cross origins

--- src/main/java/com/bos/transaction/controller/TransactionController.java
+++ src/main/java/com/bos/transaction/controller/TransactionController.java
@@ -1,18 +1,13 @@
 package com.bos.transaction.controller;
 
 import bca.bit.proj.library.base.ResultEntity;
-import com.bos.transaction.model.dao.TransactionDetailDao;
 import com.bos.transaction.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bos")
+@CrossOrigin(origins = {"*"})
 public class TransactionController {
     @Autowired
     TransactionService g_transactionService;
